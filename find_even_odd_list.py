f749654cefc5971ddbbc6362a4b7860e9a809a85
chetan-92 <chetan.bansal1992@gmail.com>
2024-02-08 22:29:00 +0530

    Added file via upload

--- find_even_odd_list.py
+++ find_even_odd_list.py
@@ -1,8 +1,37 @@
 #Code to find the even and odd numbers in a given list
+#code to chcek if a given single value is even or odd
+a = 12
 
-cif_list = [['a','A',1],['b','B',2]]
+if a%2 == 0:
+    print("even")
+else:
+    print("odd")
 
-for element in cif_list:
-    filename = '%s_%s_%s.csv' %(element[0],element[1],element[2])
-    print(filename)
 
+#code to check if the given list has all even or all odd or mixed values
+
+def check_even_odd(lst):
+    has_even = False
+    has_odd = False
+
+    for element in lst:
+        if element%2 == 0:
+            has_even = True
+        else:
+            has_odd = True
+
+    if has_even and has_odd:
+        return "Mixed"
+    elif has_even:
+        return "even"
+    else:
+        return "odd"
+
+# Example usage:
+my_list1 = [2, 4, 6, 8]
+my_list2 = [1, 3, 5, 7]
+my_list3 = [2, 3, 4, 5]
+
+print("List 1:", check_even_odd(my_list1))
+print("List 2:", check_even_odd(my_list2))
+print("List 3:", check_even_odd(my_list3))
